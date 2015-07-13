e0cd2d782013e5cf00f562e44104c5d421bce3c5
asteph <alissa.stephens1@gmail.com>
2015-07-13 10:29:25 -0500

    working with else if/ else statements

--- if.php
+++ if.php
@@ -44,4 +44,38 @@
      echo "$b is not identical to $c\n";
  }
 
+ echo 'ELSE EXERCISES' . PHP_EOL; 
+
+ // Shorten the next 2 statements into an if/else
+ if ($a < $b) {
+     echo "$a is less than $b\n";
+ }else{
+     echo "$b is greater than or equal to $a\n";   
+ }
+
+ // Shorten the next 2 statements into an if/else
+ if ($b >= $c) {
+     // output the appropriate result
+     echo "$b is greater than or equal to $c\n";
+ }else{
+     echo "$b is less than $c\n";
+ }
+
+ // combine the next 4 conditionals into
+ // one if/else/elseif block that checks in order for:
+ // identical, equal, not identical, not equal
+ if ($b === $c) {
+     // output the appropriate result
+     echo "$b is identical to $c\n";
+ }else if($b == $c){
+     echo "$b is equal to $c\n";
+ }else if ($b !== $c) {
+     // output the appropriate result
+     echo "$b is not identical to $c\n";
+ }else if ($b != $c) {
+     // output the appropriate result
+     echo "$b is not equal to $c\n";
+ }
+
+
  ?>
