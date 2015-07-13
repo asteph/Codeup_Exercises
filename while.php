90abe31c5728a39f9935dc900126c7add0e99e72
asteph <alissa.stephens1@gmail.com>
2015-07-13 11:13:56 -0500

    modified while.php exercise that will test to see if two numbers are multiples of the numbers between 5 and 15 and return if which number is or if both are

--- while.php
+++ while.php
@@ -1,7 +1,17 @@
 <?php 
 	$test = 5;
+	$multiple1 = 5;
+	$multiple2 = 2;
 	while ($test <= 15) {
-	    echo "{$test}\n";
+		if($test % $multiple1 == 0 && $test % $multiple2 == 0){
+			echo "{$test} is divisible by both {$multiple1} and {$multiple2}\n";
+		}elseif($test % $multiple1 == 0){
+			echo "{$test} is divisible by {$multiple1}\n";
+		}elseif($test % $multiple2 == 0){
+			echo "{$test} is divisible by {$multiple2}\n";
+		}else{
+	    	echo "{$test}\n";
+		}
 	    $test++;
 	}
 ?>
