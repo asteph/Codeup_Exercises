8dfd94e29d8f2fbf9961070f4f179aa24eced342
asteph <alissa.stephens1@gmail.com>
2015-07-14 11:34:28 -0500

    makes sure users are only inputing numbers for the range, and prompts them to if they do not on the first go

--- for.php
+++ for.php
@@ -1,14 +1,22 @@
 <?php
-fwrite(STDOUT, 'Starting number: ');
-$startNumber = (int)fgets(STDIN);
-fwrite(STDOUT, 'Ending number: ');
-$endNumber = (int)fgets(STDIN);
+$firstTry = true;
+do{
+	if(!$firstTry){
+		fwrite(STDOUT, 'Make sure you are using numbers.' . PHP_EOL);
+	}
+	fwrite(STDOUT, 'Starting number: ');
+	$startNumber = trim(fgets(STDIN));
+	fwrite(STDOUT, 'Ending number: ');
+	$endNumber = trim(fgets(STDIN));
+	$firstTry = false;
+}while(!is_numeric($startNumber) || !is_numeric($endNumber));
+
 fwrite(STDOUT, 'Increment by: ');
 $increment = (int)fgets(STDIN);
 if($increment == 0){
 	$increment = 1;
 }
-for($i = $startNumber; $i <= $endNumber; $i += $increment){
+for($i = (int)$startNumber; $i <= (int)$endNumber; $i += $increment){
 	echo $i . PHP_EOL;
 }
 ?>
