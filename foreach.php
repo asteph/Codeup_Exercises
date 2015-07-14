f3bdc937e7aaed334803565ccd6f6867a5ef623d
asteph <alissa.stephens1@gmail.com>
2015-07-14 14:57:45 -0500

    used implode to handle array

--- foreach.php
+++ foreach.php
@@ -2,11 +2,7 @@
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 foreach($things as $thing){
 	if(is_array($thing)){
-		echo "array( ";
-		foreach($thing as $value){
-			echo "$value ";
-		}
-		echo ")" . PHP_EOL;
+		echo "array(" . implode(', ', $thing) . ")" . PHP_EOL;
 	}else{
 		echo $thing . PHP_EOL;
 	}
