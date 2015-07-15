fb62083f3c25251eeab1f63cad92595ba15b57e0
asteph <alissa.stephens1@gmail.com>
2015-07-15 11:31:20 -0500

    echos functions for adding, subtracting, multiplying and dividing

--- arithmetic.php
+++ arithmetic.php
@@ -2,22 +2,24 @@
 
 function add($a, $b)
 {
-    echo $a + $b;
+    return $a + $b;
 }
 
 function subtract($a, $b)
 {
-    // Add code here
+	return $a - $b;
 }
 
 function multiply($a, $b)
 {
-    // Add code here
+    return $a * $b;
 }
 
 function divide($a, $b)
 {
-    // Add code here
+    return $a / $b;
 }
-
-// Add code to test your functions here
+echo add(4, 2) . PHP_EOL;
+echo subtract(4, 2) . PHP_EOL;
+echo multiply(4, 2) . PHP_EOL;
+echo divide(4, 2) . PHP_EOL;
