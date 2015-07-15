4e3e40b7c5faaba21119f4e8c49ee1eff35f1a60
asteph <alissa.stephens1@gmail.com>
2015-07-15 13:14:38 -0500

    displays an error if the input is not numberic

--- arithmetic.php
+++ arithmetic.php
@@ -1,29 +1,43 @@
 <?php
 $a = 6;
 $b = 3;
-function add($a, $b)
-{
-    return $a + $b;
+function add($a, $b){
+	if (is_numeric($a) && is_numeric($b)){
+		return $a + $b;
+	}else{
+		return "ERROR: Both arguments must be numbers \n";
+	}
+}
+function subtract($a, $b){
+	if (is_numeric($a) && is_numeric($b)){
+		return $a - $b;
+	}else{
+		return "ERROR: Both arguments must be numbers \n";
+	}
+}
+function multiply($a, $b){
+	if (is_numeric($a) && is_numeric($b)){
+		return $a * $b;
+	}else{
+		return "ERROR: Both arguments must be numbers \n";
+	}
+}
+function divide($a, $b){
+	if (is_numeric($a) && is_numeric($b)){
+		return $a / $b;
+	}else{
+		return "ERROR: Both arguments must be numbers \n";
+	}
+}
+function modulus($a, $b){
+	if (is_numeric($a) && is_numeric($b)){
+		return $a % $b;
+	}else{
+		return "ERROR: Both arguments must be numbers \n";
+	}
 }
 
-function subtract($a, $b)
-{
-	return $a - $b;
-}
 
-function multiply($a, $b)
-{
-    return $a * $b;
-}
-
-function divide($a, $b)
-{
-    return $a / $b;
-}
-function modulus($a, $b)
-{
-    return $a % $b;
-}
 echo add(4, 2) . PHP_EOL;
 echo subtract(4, 2) . PHP_EOL;
 echo multiply($a, $b) . PHP_EOL;
