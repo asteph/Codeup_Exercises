a60bc4d63bae4026d0980f36ec9e880d5be98372
asteph <alissa.stephens1@gmail.com>
2015-07-15 13:24:51 -0500

    validated divide by 0 errors and displayed if attempt to is made

--- arithmetic.php
+++ arithmetic.php
@@ -5,41 +5,43 @@ function add($a, $b){
 	if (is_numeric($a) && is_numeric($b)){
 		return $a + $b;
 	}else{
-		return "ERROR: Both arguments must be numbers \n";
+		return "ERROR: Both arguments must be numbers.";
 	}
 }
 function subtract($a, $b){
 	if (is_numeric($a) && is_numeric($b)){
 		return $a - $b;
 	}else{
-		return "ERROR: Both arguments must be numbers \n";
+		return "ERROR: Both arguments must be numbers.";
 	}
 }
 function multiply($a, $b){
 	if (is_numeric($a) && is_numeric($b)){
 		return $a * $b;
 	}else{
-		return "ERROR: Both arguments must be numbers \n";
+		return "ERROR: Both arguments must be numbers.";
 	}
 }
 function divide($a, $b){
-	if (is_numeric($a) && is_numeric($b)){
+	if(!$b){
+		return "ERROR: Can not divide by 0. Undefined.";
+	}else if (is_numeric($a) && is_numeric($b)){
 		return $a / $b;
 	}else{
-		return "ERROR: Both arguments must be numbers \n";
+		return "ERROR: Both arguments must be numbers.";
 	}
 }
 function modulus($a, $b){
 	if (is_numeric($a) && is_numeric($b)){
 		return $a % $b;
 	}else{
-		return "ERROR: Both arguments must be numbers \n";
+		return "ERROR: Both arguments must be numbers.";
 	}
 }
 
 
-echo add(4, 2) . PHP_EOL;
+echo add(1, 2) . PHP_EOL;
 echo subtract(4, 2) . PHP_EOL;
 echo multiply($a, $b) . PHP_EOL;
-echo divide($a, $b) . PHP_EOL;
+echo divide($a, 0) . PHP_EOL;
 echo modulus($a, $b) . PHP_EOL;
