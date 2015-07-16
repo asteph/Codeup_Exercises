d75e6ff9850f6374c7bc9077038b0a2dea1b751e
asteph <alissa.stephens1@gmail.com>
2015-07-16 12:06:56 -0500

    added function that compares 2 arrays and returns the number of values that match

--- search-arrays.php
+++ search-arrays.php
@@ -2,16 +2,46 @@
 
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
-$compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+$compare = ['Tina', 'Dana', 'Mel', 'Amy', 'Michael'];
 
 function isFound($item, $array){
 	$result = array_search($item, $array);
 	if($result === false){
-		return 'FALSE' . PHP_EOL;
+		return false;
 	}else{
-		return 'TRUE' . PHP_EOL;
+		return true;
 	}
 }
 
-echo isFound('Mel', $names);
-echo isFound('Mel', $compare);
+if(isFound('Mel', $names)){
+	echo 'True' . PHP_EOL;
+}else{
+	echo 'False' . PHP_EOL;
+}
+if(isFound('Mel', $compare)){
+	echo 'True' . PHP_EOL;
+}else{
+	echo 'False' . PHP_EOL;
+}
+
+function numberOfMatches($array1, $array2){
+	$matches = 0;
+	if(count($array1) > count($array2)){
+		$arrayBig = $array1;
+		$arrayLittle = $array2;
+	}else{
+		$arrayBig = $array2;
+		$arrayLittle = $array1;
+	}
+	foreach ($arrayBig as $value) {
+		$match = array_search($value, $arrayLittle);
+		if($match !== false){
+			$matches++;
+		}
+	}
+	return $matches . PHP_EOL;
+}
+
+echo numberOfMatches($names, $compare);
+
+?>
