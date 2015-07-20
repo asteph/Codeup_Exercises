0b4c8f8a5b227fcc5b7106ac97aba06e3f056ac4
asteph <alissa.stephens1@gmail.com>
2015-07-20 10:16:44 -0500

    logger has two functions for info or error that take in a message and output date/time/level/message

--- logger.php
+++ logger.php
@@ -4,15 +4,18 @@ function logMessage($logLevel, $message)
 {
 	$date = date("Y-m-d");
 	$time = date("h:i:sa");
-	$log = "$date $time ";
+	$log = "$date $time";
 	$filename = $date . '.txt';
 	//read file and parse contacts
     $handle = fopen($filename, 'a');
-    fwrite($handle, PHP_EOL . $log . $message);
-    // todo - complete this function
+    fwrite($handle, PHP_EOL . "$log $logLevel: $message");
 }
-
-logMessage("INFO", "This is an info message.");
-logMessage("ERROR", "This is an error message.");
-
+function logInfo($message){
+	logMessage("INFO", $message);
+}
+function logError($message){
+	logMessage("ERROR", $message);
+}
+logInfo('Here is some more info.');
+logError('Oh nose, an error!');
 ?>
